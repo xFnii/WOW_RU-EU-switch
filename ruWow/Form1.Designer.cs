a5cb1e1dbb5a94a1e6020f0d04359481f02ed663
home <home@DESKTOP-P7PC93R>
2017-11-20 18:17:03 +0300

    Добавьте файлы проекта.

---
+++ ruWow/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ruWow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.swap = new System.Windows.Forms.Button();
+            this.euL = new System.Windows.Forms.Button();
+            this.ruL = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(20, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RU";
+            // 
+            // swap
+            // 
+            this.swap.Location = new System.Drawing.Point(27, 48);
+            this.swap.Name = "swap";
+            this.swap.Size = new System.Drawing.Size(75, 23);
+            this.swap.TabIndex = 1;
+            this.swap.Text = "SWAP";
+            this.swap.UseVisualStyleBackColor = true;
+            this.swap.Click += new System.EventHandler(this.swap_Click);
+            // 
+            // euL
+            // 
+            this.euL.Location = new System.Drawing.Point(7, 84);
+            this.euL.Name = "euL";
+            this.euL.Size = new System.Drawing.Size(54, 23);
+            this.euL.TabIndex = 2;
+            this.euL.Text = "EU";
+            this.euL.UseVisualStyleBackColor = true;
+            this.euL.Click += new System.EventHandler(this.euL_Click);
+            // 
+            // ruL
+            // 
+            this.ruL.Location = new System.Drawing.Point(70, 84);
+            this.ruL.Name = "ruL";
+            this.ruL.Size = new System.Drawing.Size(54, 23);
+            this.ruL.TabIndex = 3;
+            this.ruL.Tag = "RU";
+            this.ruL.Text = "RU";
+            this.ruL.UseVisualStyleBackColor = true;
+            this.ruL.Click += new System.EventHandler(this.ruL_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(131, 119);
+            this.Controls.Add(this.ruL);
+            this.Controls.Add(this.euL);
+            this.Controls.Add(this.swap);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Смена языка вовна";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button swap;
+        private System.Windows.Forms.Button euL;
+        private System.Windows.Forms.Button ruL;
+    }
+}
+
---
+++ ruWow/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace ruWow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                StreamReader configWTF = new StreamReader(@"WTF\config.WTF");
+                string text = configWTF.ReadToEnd();
+                configWTF.Close();
+                if (text.Contains("SET textLocale \"ruRU\""))
+                {
+                    label1.Text = "RU NOW";
+                    return;
+                }
+                if (text.Contains("SET textLocale \"enUS\""))
+                {
+                    label1.Text = "EU NOW";
+                    return;
+                }
+            }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                label1.Text = "Не там";
+                label1.ForeColor = System.Drawing.Color.Red;
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                label1.Text = "Не там";
+                label1.ForeColor = System.Drawing.Color.Red;
+            }
+
+
+
+        }
+
+
+        private void swap_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader configWTF = new StreamReader(@"WTF\config.WTF");
+                string text = configWTF.ReadToEnd();
+                configWTF.Close();
+                if (text.Contains("SET textLocale \"ruRU\""))
+                {
+                    text = text.Replace("SET textLocale \"ruRU\"", "SET textLocale \"enUS\"");
+                    StreamWriter configWTFW = new StreamWriter(@"WTF\config.WTF");
+                    configWTFW.Write(text);
+                    configWTFW.Close();
+                    label1.Text = "EU NOW";
+                }
+                if (text.Contains("SET textLocale \"enUS\""))
+                {
+                    text = text.Replace("SET textLocale \"enUS\"", "SET textLocale \"ruRU\"");
+                    StreamWriter configWTFW = new StreamWriter(@"WTF\config.WTF");
+                    configWTFW.Write(text);
+                    label1.Text = "RU NOW";
+                    configWTFW.Close();
+                }
+            }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+
+            }
+        }
+
+        private void euL_Click(object sender, EventArgs e)
+        {
+            StreamReader configWTF = new StreamReader(@"WTF\config.WTF");
+            string text = configWTF.ReadToEnd();
+            configWTF.Close();
+            text = text.Replace("SET textLocale \"ruRU\"", "SET textLocale \"enUS\"");
+            StreamWriter configWTFW = new StreamWriter(@"WTF\config.WTF");
+            configWTFW.Write(text);
+            configWTFW.Close();
+            Process.Start("Wow-64.exe");
+            Application.Exit();
+
+        }
+
+        private void ruL_Click(object sender, EventArgs e)
+        {
+            StreamReader configWTF = new StreamReader(@"WTF\config.WTF");
+            string text = configWTF.ReadToEnd();
+            configWTF.Close();
+            text = text.Replace("SET textLocale \"enUS\"", "SET textLocale \"ruRU\"");
+            StreamWriter configWTFW = new StreamWriter(@"WTF\config.WTF");
+            configWTFW.Write(text);
+            configWTFW.Close();
+            Process.Start("Wow-64.exe");
+            Application.Exit();
+        }
+    }
+}
